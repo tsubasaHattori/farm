68e00c081eec878ab5e28f3f00b88f0584561bde
tsubasaHattori <fourmoonside@gmail.com>
2020-04-27 03:09:08 +0900

    fix migration

--- database/migrations/2020_04_27_030625_add_various_columns_users_table.php
+++ database/migrations/2020_04_27_030625_add_various_columns_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsUsersTable extends Migration
+class AddVariousColumnsUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -36,3 +36,4 @@ class AddColumnsUsersTable extends Migration
         });
     }
 }
+
